8c3ff3873aa165f78ff5f3ccfea64c76636e22ea
Rahul <rahuls0599@gmail.com>
2020-07-01 14:34:39 +0530

    Scraping Playlist Captions in 1 file

--- scraping_playlist_captions.py
+++ scraping_playlist_captions.py
@@ -1,23 +1,43 @@
-"""This script accepts a YouTube Video id,
-and stores the available captions (if any) in a file at current working directory"""
-
-from youtube_transcript_api import YouTubeTranscriptApi
-
-# provide the video id- the part after v=...
-VIDEO_ID = r'PPD8lER8ju4&list=PLH2l6uzC4UEW3iJO4T0qUeUEp_X-f1U7S&index=23'
+"""This script accepts a YouTube Video id from a collection of Video IDs read from a file,
+and stores the available captions for each video (if any) in a file at current working directory"""
 
 # provide the name of output file in quotes
-FILE_NAME = r"/mnt/c/Users/rahul/Documents/Tutorials/Probability and Statistics/CrashCourseStatistics_21-P-Value Problems"
+# importing the libraries 
+from youtube_transcript_api import YouTubeTranscriptApi
+from bs4 import BeautifulSoup 
+import requests 
 
-CAPTIONS = YouTubeTranscriptApi.get_transcript(VIDEO_ID)
-TOTAL_CAPTIONS = "URL = https://www.youtube.com/watch?v="+VIDEO_ID+"\n"
+PLAYLIST_NAME = "CrashCourseStatistics"
+FILE_NAME = r"/mnt/c/Users/rahul/Documents/Tutorials/Probability and Statistics/" + PLAYLIST_NAME
 
-for word in CAPTIONS:
-	TOTAL_CAPTIONS = TOTAL_CAPTIONS + word['text'] + "\n"
-
-
-TEXT_FILE = open(FILE_NAME+".txt", "w")
-TEXT_FILE.write(TOTAL_CAPTIONS)
-TEXT_FILE.close()
-
-with open("video.csv")
+with open("video_ids.csv") as video_id_list:
+    for VIDEO_ID in video_id_list:
+    
+        #Forming the complete url of the video
+        URL = "https://www.youtube.com/watch?v="+VIDEO_ID
+        
+        #getting the captions
+        CAPTIONS = YouTubeTranscriptApi.get_transcript(VIDEO_ID)
+        
+        # getting the request from url 
+        # FLAG_R = requests.get(URL) 
+	
+        # converting the text 
+        # FLAG_S = BeautifulSoup(FLAG_R.text, "html.parser") 
+	
+        # finding meta info for title 
+        # TITLE = FLAG_S.find("h1", class_="style-scope ytd-video-primary-info-renderer").children[0].text.replace("\n", "") 
+        
+        #desired file name for storing the captions
+        # FILE_NAME = r"/mnt/c/Users/rahul/Documents/" + "CrashCourseStatistics"
+        
+        #storing the name of video and its url as initials
+        TOTAL_CAPTIONS = "URL = " + URL + "\n"  # + "TITLE = " + TITLE + "\n" 
+        
+        
+        for word in CAPTIONS:
+            TOTAL_CAPTIONS = TOTAL_CAPTIONS + word['text'] + "\n"
+        
+        TEXT_FILE = open(FILE_NAME+".txt", "a")
+        TEXT_FILE.write(TOTAL_CAPTIONS)
+        TEXT_FILE.close()
